7e3ae0a446d5df100e5c764a82064037b9510e61
e-danziger <eduard.danziger@googlemail.com>
2025-07-22 21:31:45 +0200

    LogBuffer fix

--- common/SpdLogger/LogBuffer.h
+++ common/SpdLogger/LogBuffer.h
@@ -4,11 +4,11 @@
 #include <mutex>
 #include <sstream>
 
-#include "spdlog/sinks/sink.h"
+#include <spdlog/sinks/sink.h>
 
 namespace ed::model
 {
-	class LogBuffer : public spdlog::sinks::sink
+	class LogBuffer final : public spdlog::sinks::sink
 	{
 	public:
 		LogBuffer() = default;
@@ -17,6 +17,14 @@ namespace ed::model
 		void log(const spdlog::details::log_msg& msg) override;
 
 		void flush() override
+		{
+		}
+
+		void set_pattern(const std::string& pattern) override
+		{
+		}
+
+		void set_formatter(std::unique_ptr<spdlog::formatter> sink_formatter) override
 		{
 		}
 
